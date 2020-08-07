8d63b05dd775a8002986e84be6fd358891a5edd4
Steven Winter <stevenwinter.me@gmail.com>
2020-08-07 17:00:21 -0500

    Destructured objects in app, geocode, and forecast. Practiced http request without library.

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('postman-request')
 const forecast = (lat, long, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=30698f082515ebcb6bf3577bf4811ca8&query=${lat},${long}&units=f`
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if(error) {
             callback('Unable to connect to weather services.')
-        } else if (response.body.error) { 
+        } else if (body.error) { 
             callback('Unable to find location. Try another search.')
         } else {
-            callback(undefined, response.body.current.weather_descriptions[0] + ' It is currently ' + response.body.current.temperature + ' degrees out. There is a ' + response.body.current.precip + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degrees out. There is a ' + body.current.precip + '% chance of rain.')
         }
     })
 }
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -3,16 +3,16 @@ const request = require('postman-request')
 const geocode = (address, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1Ijoic3RldmVwbGF5c2hvcm4iLCJhIjoiY2tkZ3BtcTZpMHIxbjJzdDVmMWZscG00aCJ9.TLKsTTUvEoUq7tja3bedbw&limit=1`
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if(error) {
             callback('Unable to connect to location services.')
-        } else if (response.body.features.length === 0) { 
+        } else if (body.features.length === 0) { 
             callback('Unable to find location. Try another search.')
         } else {
             callback(undefined, {
-                long: response.body.features[0].center[0],
-                lat: response.body.features[0].center[1],
-                location: response.body.features[0].place_name
+                long: body.features[0].center[0],
+                lat: body.features[0].center[1],
+                location: body.features[0].place_name
             })
         }
     })

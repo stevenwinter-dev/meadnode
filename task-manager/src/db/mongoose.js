fcc0512cc104fd05e48d6e96a751edeb36dc8ba5
Steven Winter <stevenwinter.me@gmail.com>
2020-08-11 22:41:37 -0500

    Removed mongoose.js and replaced with task.js and user.js in model folder. Added create and read API endpoints.

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,79 +1,8 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
-})
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value) {
-            if(value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot contain password')
-            }
-        }
-    },
-    email: {
-        type: String,
-        required: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
-    }
-})
-
-// const me = new User({
-//     name: 'Steven',
-//     email: 'steveplayshorn@gmail.com',
-//     password: '    1231234'
-// })
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error', error)
-// })
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
-
-const teach = new Task({
-    description: 'Teach',
-    completed: false
-})
-
-teach.save().then(() => {
-    console.log(teach)
-}).catch((error) => {
-    console.log('Error', error)
 })

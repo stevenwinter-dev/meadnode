4ced12f156db7b17a0851b3b945a94dcd0cddc57
Steven Winter <stevenwinter.me@gmail.com>
2020-08-13 23:17:21 -0500

    Added the remaining routes and moved all routes to routers folder.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,81 +1,15 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
 const Task = require('./models/task')
-const { findById } = require('./models/user')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
-
-    try {
-        await user.save()
-        res.send(201).send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-    
-})
-
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send()    
-    }
-})
-
-app.get('/users/:id', async (req, res) => {
-    const _id =  req.params.id
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
-
-    try {
-        await task.save()
-        res.status(201).send(task)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-app.get('/tasks', async (req, res) => {
-    const tasks = await Task.find({})
-    res.send(tasks)
-    Task.find({}).then((tasks) => {
-        res.send(tasks)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
-
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
-
-    Task.findById(_id).then((task) => {
-        if(!task) {
-            return res.status(404).send()
-        }
-        res.send(task)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
+app.use(userRouter, taskRouter)
 
 app.listen(port, () => {
     console.log('Server is listening to ' + port)

050f100d8d1fbd8c28140611870ca23efbd92be2
Steven Winter <stevenwinter.me@gmail.com>
2020-08-27 19:39:10 -0500

    Added middleware for maintenance mode.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,6 +7,18 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+app.use((req, res, next) => {
+    res.status(503).send('Site is currently down. Check back soon.')
+})
+
 app.use(express.json())
 app.use(userRouter, taskRouter)
 

89454f489d5b2fe3b580e237979abd46aaef186e
Steven Winter <stevenwinter.me@gmail.com>
2020-09-03 21:38:23 -0500

    Added middleware for user route.

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+//C:\Users\Steven\mongodb\bin\mongod.exe --dbpath=\Users\Steven\mongodb-data
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
